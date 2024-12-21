fe415fe4ab96fde93350b347243a1ae3f346f168
oussamahdidou <oussamahdidou15@gmail.com>
2024-12-21 18:20:21 +0100

    add structural patterns and behavioral patterns

---
+++ C#/Chain of Responsibility/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Chain_of_Responsibility
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Reads the content of a file from the specified path.
+        /// </summary>
+        /// <param name="filePath">The path of the file to read.</param>
+        /// <returns>The content of the file as a string.</returns>
+        /// <exception cref="FileNotFoundException">Thrown when the file is not found.</exception>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ C#/Template Method/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Template_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
